02d16f0e8ec9c8af249182522bdb8c9e6bb94b18
nahidbd11 <messibossbarca@gmail.com>
2022-04-08 11:24:04 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-7tajqp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-7tajqp)
---
+++ index.js
@@ -0,0 +1,18 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+//Create an array with just length and elements are undefine
+const arr = [...new Array(5)];
+console.log(arr);
+
+//Array.from
+
+let a = Array.from({ length: 6 }, (v, i) => {
+  console.log(v, i);
+  return v;
+});
+console.log(a);
